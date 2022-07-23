21a3cc9353d8af694f5ce00d0c1d1915caa8543f
senhai-kun <senpaikouhai04@gmail.com>
2022-07-23 19:48:59 +0800

    request limit to 20

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post("/suggestions", (req, res) => {
 app.post("/search", (req, res) => {
     const { query } = req.body
 
-    YouTube.search( query , { limit: 21 } )
+    YouTube.search( query , { limit: 20, safeSearch: true } )
     .then( i => {
         let result = i.map( (i) => {
             return {
